6654ac5378f98772116ca0dcd00ac124d2ffb8cd
Zuhra Rashidovna <z.khasanzyanova@yandex.ru>
2024-06-12 07:36:00 +0300

    Поправила checkStyle

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -2,16 +2,17 @@ package service;
 
 import java.io.File;
 
-public class Managers  {
+public class Managers {
     public static FileBackedTaskManager getFileBackedTaskManager() {
         return new FileBackedTaskManager(new File("resourses/taskT.csv"));
     }
 
-    public static TaskManager getDefaultTaskManager(){
+    public static TaskManager getDefaultTaskManager() {
 
         return new InMemoryTaskManager(new InMemoryHistoryManager());
     }
-    public static HistoryManager getDefaultHistory(){
+
+    public static HistoryManager getDefaultHistory() {
 
         return new InMemoryHistoryManager();
     }
